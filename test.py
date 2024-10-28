e3df37e3960892878147999de2b65ba97706482d
Artsem <72626349+ArtsemNikulin@users.noreply.github.com>
2024-10-28 14:38:34 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ print('Hello arch')
 
 
 conn = pymssql.connect(
-    server='192.168.52.249',
+    server='192.168.52.6',
     user='test',
     password='Qazxcvfr1234',
     database='AdventureWorks2022',
