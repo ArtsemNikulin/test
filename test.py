9aa81a1fbf1f8e85a057301498fb010ed6f8dfe8
Artsem <72626349+ArtsemNikulin@users.noreply.github.com>
2024-10-28 13:53:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ print('Hello arch')
 
 
 conn = pymssql.connect(
-    server='host.docker.internal',
+    server='EPPLWARW009D\SQLEXPRESS',
     user='test',
     password='Qazxcvfr1234',
     database='AdventureWorks2022',
