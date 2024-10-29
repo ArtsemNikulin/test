fa22041609fe175d1032f100fff0ed53c3e56796
Artsem <72626349+ArtsemNikulin@users.noreply.github.com>
2024-10-29 09:14:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
-import pymssql
-print('Hello arch')
+# import pymssql
+# print('Hello arch')
 
 
-conn = pymssql.connect(
-    server='192.168.0.53',
-    user='test',
-    password='Qazxcvfr1234',
-    database='AdventureWorks2022',
-    as_dict=True
-)
-cursor = conn.cursor()
-cursor.execute('SELECT TOP 10 * FROM person.person')
-records = cursor.fetchall()
-print(records)
-conn.close()
+# conn = pymssql.connect(
+#     server='192.168.0.53',
+#     user='test',
+#     password='Qazxcvfr1234',
+#     database='AdventureWorks2022',
+#     as_dict=True
+# )
+# cursor = conn.cursor()
+# cursor.execute('SELECT TOP 10 * FROM person.person')
+# records = cursor.fetchall()
+# print(records)
+# conn.close()
+import pyodbc
+
+with pyodbc.connect(r'DRIVER={SQL Server};Server=host.docker.internal;Database=AdventureWorks2022;UID=test;PWD=Qazxcvfr1234',
+                    autocommit=True) as conn:
+    cursor = conn.cursor()
+    x = list(cursor.execute('SELECT TOP 10 * FROM person.person'))
+    print(x)
